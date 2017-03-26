90c04bae38e7bab25183e19f089720e6fa4f9f77
Ovchinnikov Dmitriy <ovchinnikov.dk@phystesh.edu>
2017-03-26 23:22:23 +0300

    third

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 hadoop fs -rm -r out >/dev/null 2>&1
-hadoop jar jar/UrlCount.jar ru.mipt.hobod.hw2.MainClass /data/stackexchange/posts /data/stackexchange/users out >/dev/null 2>&1
+hadoop jar jar/hw2.jar ru.mipt.hobod.hw2.MainClass /data/stackexchange/posts /data/stackexchange/users out >/dev/null 2>&1
 hadoop fs -cat out/part-r-00000 | head -40
