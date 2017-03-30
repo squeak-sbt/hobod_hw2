d4a66058950a9bc0c4f848d8c47794b5f4062eb6
Dmitriy Ovchinnikov <ovchinnikov.dk@phystech.edu>
2017-03-30 23:27:01 +0200

    final version

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 hadoop fs -rm -r out tmp >/dev/null 2>&1
 hadoop jar jar/hw2.jar ru.mipt.hobod.hw2.job1_join.JobOneMain /data/stackexchange/posts /data/stackexchange/users out >/dev/null 2>&1
-hadoop jar jar/hw2.jar ru.mipt.hobod.hw2.job2_grouping.JobTwoMain out tmp >/dev/null
-#hadoop fs -cat out/part-r-00000 | head -40
+hadoop jar jar/hw2.jar ru.mipt.hobod.hw2.job2_grouping.JobTwoMain out tmp
+
