8f79801ff343fac0a840663f7a3675155670d47e
Ovchinnikov Dmitriy <ovchinnikov.dk@phystesh.edu>
2017-03-30 19:30:39 +0300

    reduce side join

--- src/ru/mipt/hobod/hw2/reducer/MyReducer.java
+++ src/ru/mipt/hobod/hw2/reducer/MyReducer.java
@@ -1,32 +1,21 @@
 package ru.mipt.hobod.hw2.reducer;
 
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by dmitry on 26.03.17.
  */
 public class MyReducer extends Reducer<Text, Text, Text, Text> {
 
+
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        /*int reputation = 0;
-        int maxScore = 0;
-        for (Text value : values) {
-            String[] params = value.toString().split(",");
-            if (params[0].equals("3")) {
-                reputation = Integer.valueOf(params[1]);
-            }
-            if (params[0].equals("1")) {
-
-            }
-        }*/
-        /*for (Text value : values) {
-            context.write(value, NullWritable.get());
-        }*/
+        List<String[]> answers = new ArrayList<>();
         String reputation = null;
         boolean found = false;
         for (Text value : values) {
@@ -34,16 +23,15 @@ public class MyReducer extends Reducer<Text, Text, Text, Text> {
             if (split[0].equals("U")) {
                 reputation = split[1];
                 found = true;
-                context.write(new Text("SUCCESS"), new Text());
                 break;
+            }
+            else {
+                answers.add(split);
             }
         }
         if (found) {
-            for (Text value : values) {
-                String[] attributes = value.toString().split(",");
-                if (attributes[0].equals("A")) {
-                    context.write(new Text(attributes[1]), new Text(attributes[2] + "\t" + reputation));
-                }
+            for (String[] attributes : answers) {
+                context.write(new Text(attributes[1]), new Text(attributes[2] + "\t" + reputation));
             }
         }
     }
